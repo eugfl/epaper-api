118e00abc611749870199a719b6cd6d002a3d377
Gabriel Figueiredo <eugfl@hotmail.com>
2024-11-19 22:57:18 -0300

    :sparkles: feat(s3): Add S3 service integration with MinIO using AWS SDK

---
+++ s3.service.ts
@@ -0,0 +1,13 @@
+import { S3Client } from '@aws-sdk/client-s3';
+
+const s3 = new S3Client({
+  endpoint: 'http://localhost:9000',
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId: process.env.MINIO_ACCESS_KEY,
+    secretAccessKey: process.env.MINIO_SECRET_KEY,
+  },
+  forcePathStyle: true,
+});
+
+export default s3;
